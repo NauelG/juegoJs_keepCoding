5a26e5aefac1ea528c273c14608c3f3560be68f0
Nauel Gamero <g.nauel@gmail.com>
2018-05-08 18:33:14 +0200

    Juego terminado v1.0

--- index.js
+++ index.js
@@ -1,29 +1,73 @@
-let plan = [
-    "                                                                                ",
-    "                                                                                ",
-    "                                                                                ",
-    "                                                                                ",
-    "                                                                                ",
-    "                                                                                ",
-    "                                                                  xxx           ",
-    "                                                   xx      xx    xx!xx          ",
-    "                                    o o      xx                  x!!!x          ",
-    "                                                                 xx!xx          ",
-    "                                   xxxxx                          xvx           ",
-    "                                                                            xx  ",
-    "  xx                                      o o                                x  ",
-    "  x                     o                                                    x  ",
-    "  x                                      xxxxx                             o x  ",
-    "  x          xxxx       o                                                    x  ",
-    "  x  @       x  x                                                xxxxx       x  ",
-    "  xxxxxxxxxxxx  xxxxxxxxxxxxxxx   xxxxxxxxxxxxxxxxxxxx     xxxxxxx   xxxxxxxxx  ",
-    "                              x   x                  x     x                    ",
-    "                              x!!!x                  x!!!!!x                    ",
-    "                              x!!!x                  x!!!!!x                    ",
-    "                              xxxxx                  xxxxxxx                    ",
-    "                                                                                ",
-    "                                                                                "
-];
-let level = new Level(plan);
+const ARROW_CODES = {
+    37: 'left',
+    38: 'up',
+    39: 'right'
+}
 
-let display = new DOMDisplay(document.body, level)
+let arrows = trackKews(ARROW_CODES);
+
+function trackKews(keyCodes) {
+    let pressedKeys = {};
+
+    function handler(event) {
+        if (keyCodes.hasOwnProperty(event.keyCode)) {
+            let downPressed = event.type === 'keydown';
+            pressedKeys[keyCodes[event.keyCode]] = downPressed;
+            event.preventDefault();
+        }
+    };
+    addEventListener('keydown', handler);
+    addEventListener('keyup', handler);
+
+    return pressedKeys;
+}
+
+function runAnimation(frameFunction) {
+    let lastTime = null;
+
+    function frame(time) {
+        let stop = false;
+        if (lastTime !== null) {
+            timeStep = Math.min(time - lastTime, 100) / 1000;
+            stop = frameFunction(timeStep) === false;
+        }
+        lastTime = time;
+        if (!stop) requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
+}
+
+function runLevel(level, Display, callback) {
+    let display = new Display(document.body, level);
+    runAnimation(function(step) {
+        level.animate(step, arrows);
+        display.drawFrame();
+        if (level.isFinished()) {
+            display.clear();
+            if (callback) callback(level.status);
+            return false;
+        }
+    });
+}
+
+function runGame(levels, Display) {
+    function startLevel(levelNumber) {
+        let levelObject;
+        try {
+            levelObject = new Level(levels[levelNumber]);
+        } catch (error) {
+            return alert(error.message);
+        }
+
+        runLevel(levelObject, Display, status => {
+            if (status === 'lost') startLevel(levelNumber);
+            else if (levelNumber < levels.length - 1) startLevel(++levelNumber);
+            else alert('¡¡Has ganado!!')
+        });
+    };
+
+    startLevel(0);
+
+}
+
+runGame(GAME_LEVELS, DOMDisplay);
